f4e603bb9ad01175de3a829df29623679a44c6f9
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-10 15:54:53 -0800

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react-refresh", "react", "react-hooks"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-explicit-any": ["off"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
