194d1fb6a926b60ee6a90af8072cf7fbf75cbaf9
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-17 13:25:10 -0800

    tailwind congfig

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className='text-red-400'>
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
