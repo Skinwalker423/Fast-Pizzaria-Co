e070f5fd62d5ae855ac1654efea1d7d68a320ba0
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-17 14:33:02 -0800

    prettier congig

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <h1 className='text-red-400'>
+      <h1 className="text-center text-xl font-semibold text-red-400">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
