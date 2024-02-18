beb297121ecfa6f015381b01e2eceac930a17117
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-17 16:09:24 -0800

    Username component

--- src/features/cart/CartOverview.tsx
+++ src/features/cart/CartOverview.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-900 text-stone-100">
+    <div className="bg-stone-800 uppercase text-stone-200">
       <p className="text-stone-300">
         <span>23 pizzas</span>
         <span>$23.45</span>
--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 const Header = () => {
   return (
-    <header className="bg-yellow-500 px-2 py-4">
-      <Link to={"/"}>Fast React Pizza Co</Link>
+    <header className="bg-yellow-500 px-2 py-4 uppercase">
+      <Link className="tracking-widest" to={"/"}>
+        Fast React Pizza Co
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 };
