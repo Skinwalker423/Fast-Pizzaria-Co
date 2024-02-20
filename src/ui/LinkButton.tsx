a2ce4fcfcc9c0db18bfc6ebae6439baa9099cbf5
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-20 13:46:41 -0800

    return button if to is -1

--- src/ui/LinkButton.tsx
+++ src/ui/LinkButton.tsx
@@ -1,14 +1,23 @@
 import { PropsWithChildren } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 interface LinkButtonProps extends PropsWithChildren {
   to: string;
 }
 
 const LinkButton: React.FC<LinkButtonProps> = ({ children, to, ...rest }) => {
+  const navigate = useNavigate();
+  if (to === "-1") {
+    return <button onClick={() => navigate(-1)}>&larr; Go back</button>;
+  }
+
   return (
-    <Link {...rest} is="button" to={to}>
-      <button>{children}</button>
+    <Link
+      className="text-sm text-blue-500 hover:text-blue-700"
+      to={to}
+      {...rest}
+    >
+      {children}
     </Link>
   );
 };
