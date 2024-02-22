3a6a2d7f7d2f830f0122106a313d105be34961f4
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-22 15:15:59 -0800

    menu item styles

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends PropsWithChildren {
   to?: string;
   color?: string;
   textColor?: string;
-  size?: string;
+  size?: "small" | "medium" | "large";
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -18,16 +18,12 @@ const Button: React.FC<ButtonProps> = ({
   to,
   ...rest
 }) => {
-  let sizes = "";
-  if (size === "medium") {
-    sizes = "px-3 py-2 sm:px-4 sm:py-2";
-  } else if (size === "small") {
-    sizes = "px-2 py-1";
-  } else {
-    sizes = "px-4 py-3 sm:px-6 sm:py-4";
-  }
-  const classes = `rounded-full bg-yellow-500 ${sizes} font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-400 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-300  ${className}`;
-
+  const sizes = {
+    small: "px-2 py-1 text-xs",
+    medium: "px-3 py-2 sm:px-4 sm:py-2 text-sm",
+    large: "px-4 py-3 sm:px-6 sm:py-4",
+  };
+  const classes = `rounded-full bg-yellow-500 ${sizes[size]} font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-400 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-300  ${className}`;
   if (to)
     return (
       <Link className={classes} to={to}>
