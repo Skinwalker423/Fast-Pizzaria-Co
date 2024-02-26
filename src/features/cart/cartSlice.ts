e589a98870f55ecdf18a92af1a63ea7fed59a33d
Luis Gonzalez <lgonzalez23@verizon.net>
2024-02-25 17:53:37 -0800

    increase cart item

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -16,10 +16,41 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action: PayloadAction<CartItemProps>) => {
       console.log(action.payload);
+      const isItemInCart = state.cart.find(
+        (item) => item.pizzaId === action.payload.pizzaId,
+      );
+      if (isItemInCart) {
+        return;
+      } else {
+        state.cart.push(action.payload);
+      }
+    },
+    removeFromCart: (state, action: PayloadAction<string>) => {
+      console.log(action.payload);
+      state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuanity: (state, action: PayloadAction<string>) => {
+      console.log(action.payload);
+      state.cart.map((item) => {
+        if (item.pizzaId === action.payload) {
+          return {
+            ...item,
+            quantity: item.quantity + 1,
+            totalPrice: item.totalPrice + item.unitPrice,
+          };
+        }
+      });
+    },
+    decreaseItemQuanity: (state, action: PayloadAction<CartItemProps>) => {
+      console.log(action.payload);
+      state.cart.push(action.payload);
+    },
+    clearCart: (state, action: PayloadAction<CartItemProps>) => {
+      console.log(action.payload);
       state.cart.push(action.payload);
     },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, increaseItemQuanity } = cartSlice.actions;
 export default cartSlice.reducer;
